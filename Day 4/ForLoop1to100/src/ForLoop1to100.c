9e9fadb566845b7f93efa2f68b380e2c4507ea0c
S.Qadir <70005344+SQADIRKVM@users.noreply.github.com>
2022-10-10 15:16:51 +0530

    Day 3 /For loop - #100DaysOfCode

---
+++ Day 4/ForLoop1to100/src/ForLoop1to100.c
@@ -0,0 +1,21 @@
+/*
+ ============================================================================
+ Name        : ForLoop1to100.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	int i;
+	setbuf(stdout,NULL);
+	for(i=1;i<=100;i++){
+		printf("%d\n",i);
+	}
+	return EXIT_SUCCESS;
+}
